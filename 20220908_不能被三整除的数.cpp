c3cedaf8fb9de04c19f068101e1022edc1835744
starynightt <113521695+starynightt@users.noreply.github.com>
2022-09-17 13:08:06 +0800

    Add files via upload

---
+++ 20220908_不能被三整除的数.cpp
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int max(int a, int b) {
+	return a > b ? a : b;
+}
+int min(int a, int b) {
+	return a > b ? b : a;
+}
+
+int main() {
+	int n, m, x, y;
+	scanf("%d%d", &n, &m);
+	x = max(n, m);
+	y = min(n, m);
+	for (; y <= x; ++y) {
+		if (y % 3 != 0)
+			printf("%d ", y);
+	}
+	printf("\n");
+	return 0;
+}
