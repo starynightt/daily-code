806860dc94d2c31cc00b7822793f0645bffdf782
starynightt <113521695+starynightt@users.noreply.github.com>
2022-09-21 15:51:04 +0800

    Add files via upload

---
+++ 20220920_猴子选大王.cpp
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+void loop(int M, int K) {
+	int i, n = 0;
+	for (i = 2; i <= M; ++i) {
+		n = (n + K) % i;
+	}
+	printf("%d", n+1);
+}
+
+int main() {
+	int M, K;
+	scanf("%d%d", &M, &K);
+	loop(M, K);
+	return 0;
+}
