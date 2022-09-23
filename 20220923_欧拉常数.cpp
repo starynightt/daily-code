5ed3e4a5e85891e8093bb064e9867e4ba1093420
starynightt <113521695+starynightt@users.noreply.github.com>
2022-09-23 20:06:01 +0800

    Add files via upload

---
+++ 20220923_欧拉常数.cpp
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<math.h>
+
+double sum(double n) {
+	double sum = 0, i;
+	for (i = 1; i <= n; ++i) {
+		sum += 1/ i;
+	}
+	return sum;
+}
+
+int main() {
+	double n, r;
+	scanf("%lf", &n);
+	r = sum(n) - log(n);
+	printf("%lf", r);
+	return 0;
+}
