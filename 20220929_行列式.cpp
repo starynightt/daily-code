3391aef4fce7c29a34ad4232e7aef7540e8c869d
starynightt <113521695+starynightt@users.noreply.github.com>
2022-09-29 10:34:21 +0800

    Add files via upload

---
+++ 20220929_行列式.cpp
@@ -0,0 +1,30 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#define PI 3.1415926
+#include<stdio.h>
+#include<math.h>
+
+double Dn(int n, double x) {
+	double a = 0, b = 0, c = 0;
+	int i;
+	a = cos(x);
+	b = cos(2 * x);
+	if (n == 1) {
+		return cos(x);
+	}
+	if (n == 2) {
+		return cos(2 * x);
+	}
+	for (i = 2; i < n; ++i) {
+		c = 2 * cos(x) * b - a;
+		a = b;
+		b = c;
+	}
+	return c;
+}
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	printf("%lf", Dn(n, PI / 3));
+	return 0;
+}
