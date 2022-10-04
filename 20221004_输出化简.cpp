9c8f1ccffba26485e3252e1689401f972dec0c2b
starynightt <113521695+starynightt@users.noreply.github.com>
2022-10-04 21:18:20 +0800

    Add files via upload

---
+++ 20221004_输出化简.cpp
@@ -0,0 +1,37 @@
+﻿#define _CRT_SECURE_NO_AWRNINGS
+#include<stdio.h>
+
+void cutspace(void) {
+	char c1;
+	do {
+		c1 = getchar();
+		if (c1 != ' ') {
+			putchar(c1);
+		}
+		if (c1 == ' ') {
+			while ((c1 = getchar()) == ' ');
+			putchar(' ');
+			putchar(c1);
+		}
+	} while (c1 != '\n');
+}
+
+void singleword(void) {
+	char c1;
+	do {
+		c1 = getchar();
+		if (c1 != ' ') {
+			putchar(c1);
+		}
+		if (c1 == ' ') {
+			while ((c1 = getchar()) == ' ');
+			putchar('\n');
+			putchar(c1);
+		}
+	} while (c1 != '\n');
+}
+
+int main() {
+	cutspace();
+	return 0;
+}
