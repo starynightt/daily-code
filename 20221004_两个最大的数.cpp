e1c59e8fd3643c7154c9bd6e7daac4f159dd6577
starynightt <113521695+starynightt@users.noreply.github.com>
2022-10-04 20:36:02 +0800

    Add files via upload

---
+++ 20221004_两个最大的数.cpp
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main() {
+	int max1 = -2147483648, max2 = -2147483648, c;
+	do {
+		scanf("%d", &c);
+		if (c >= max1) {
+			max2 = max1;
+			max1 = c;
+		}
+		if (c > max2 && c < max1) {
+			max2 = c;
+		}
+		c = getchar();
+	} while (c != '\n');
+	printf("%d %d", max1, max2);
+	return 0;
+}
