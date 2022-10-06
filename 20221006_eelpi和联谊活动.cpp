03c755fed89814a1955cf43a9b5d957c84f88dcf
starynightt <113521695+starynightt@users.noreply.github.com>
2022-10-07 07:11:18 +0800

    Add files via upload

---
+++ 20221006_eelpi和联谊活动.cpp
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+enum{NUM=100000};
+int hum[NUM] = { 0 };
+long ci[NUM] = { 0 };
+
+void scan(long ci[NUM], int n) {
+    int i, c;
+    for (i = 0; i < n - 1; ++i) {
+        scanf("%d", &c);
+        ci[i] = c;
+    }
+}
+
+int main() {
+    int n, i;
+    long long sum=0;
+    scanf("%d", &n);
+    scan(ci, n);
+    for (i = 0; i < n - 1; ++i) {
+        sum += ci[i] * (i + 2);
+    }
+    printf("%lld", sum);
+    return 0;
+}
+                               
