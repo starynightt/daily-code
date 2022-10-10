ca22896da9e6a4a4b6a9209484b98391a067099a
starynightt <113521695+starynightt@users.noreply.github.com>
2022-10-10 21:48:17 +0800

    Add files via upload

---
+++ 20221010_倒数.cpp
@@ -0,0 +1,18 @@
+﻿#include<stdio.h>
+char num[100000000];
+
+int main() {
+	int c,i;
+	for (i=0; (c = getchar()) != '\n';++i) {
+		num[i] = c;
+	}
+	if (num[0] == '-') {
+		printf("-");
+	}
+	for (; i >= 0; --i) {
+		if (num[i] != '-'&&num[i]!='0')
+			printf("%c", num[i]);
+	}
+	printf("\n");
+	return 0;
+}
