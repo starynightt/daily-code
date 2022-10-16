8dda588332a6434d44ad8f401a27bd574a388b69
starynightt <113521695+starynightt@users.noreply.github.com>
2022-10-16 11:50:52 +0800

    Add files via upload

---
+++ 20221016_斐波那契（动态规划）.cpp
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+long long fibs[100] = { 0 };
+
+long long fib(int n) {
+	if (fibs[n] !=0 ) {
+		return fibs[n];
+	}
+	else if (n == 1 || n == 2) {
+		return fibs[n] = 1;
+	}
+	else {
+		return fibs[n] = fib(n - 1) + fib(n - 2);
+	}
+}
+
+int main() {
+	int m, n,temp,i;
+	scanf("%d%d", &m, &n);
+	if (m > n) {
+		temp = m;
+		m = n;
+		n = temp;
+	}
+	for (i = n; i >=m; --i) {
+		printf("%lld ", fib(i));
+	}
+	return 0;
+}
